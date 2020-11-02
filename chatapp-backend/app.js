14f3b14cbc21d1da356dc660e31644271fd412e0
Kocsis Robert <robertzkocsis@gmail.com>
2020-11-02 22:48:12 +0100

    Remove the bin folder, and use passport authentication is socket io

--- chatapp-backend/app.js
+++ chatapp-backend/app.js
@@ -4,15 +4,15 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
 const passport = require("passport");
-const flash = require("express-flash");
 const session = require("express-session");
+const debug = require("debug")("chatapp-backend:server");
+const http = require("http");
+const socketAPI = require("./config/socketAPI");
 const initializePassport = require("./config/passport-config");
-
+const db = require("./config/db");
 const mainRouter = require("./controllers/mainRouter");
 
-const db = require("./config/db");
 const app = express();
 
 app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
@@ -22,14 +22,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(flash());
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+const sessionObject = session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false,
+});
+
+app.use(sessionObject);
+
 initializePassport(passport);
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,5 +52,81 @@ app.use(function (err, req, res, next) {
   console.log("error", err);
   res.json(err);
 });
+
+const wrap = (middleware) => (socket, next) =>
+  middleware(socket.request, {}, next);
+socketAPI.io.use(wrap(sessionObject));
+socketAPI.io.use(wrap(passport.initialize()));
+socketAPI.io.use(wrap(passport.session()));
+
+socketAPI.io.use((socket, next) => {
+  if (socket.request.user) {
+    console.log("MEGVAN A USER");
+    next();
+  } else {
+    next(new Error("unauthorized"));
+  }
+});
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+socketAPI.io.attach(server);
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
 
 module.exports = app;
